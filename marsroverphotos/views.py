49d865bc9a75ac690f45e9fde870e7e97c0f6532
Christian <christian_nagata@hotmail.com>
2022-01-06 15:01:20 -0300

    Mudança de template, load de estáticos, e criação de base.html

--- marsroverphotos/views.py
+++ marsroverphotos/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'index.html')
+    api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
+
+    info = requests.get(
+        f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={api_key}')
+
+    info = info.json()
+
+    context = {'photos': info['photos'][0]}
+
+    return render(request, 'index.html', context)
