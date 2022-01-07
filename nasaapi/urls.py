953e8ef3d77bad6b2df41a62a83598c8e01edfd3
Christian <christian_nagata@hotmail.com>
2022-01-07 13:30:18 -0300

    correção do banner na página inicial

--- nasaapi/urls.py
+++ nasaapi/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('APOD/', views.apod, name='apod'),
+    path('MarsRoverPhotos/', views.mars_rover_photos, name='mars_rover_photos'),
 ]
--- nasaapi/views.py
+++ nasaapi/views.py
@@ -6,14 +6,29 @@ api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
 
 
 def index(request):
+    """Página inicial"""
+
     return render(request, 'index.html')
 
 
 def apod(request):
+    """Fotos atronômicas do dia"""
+
     info = requests.get(
         f'https://api.nasa.gov/planetary/apod?api_key={api_key}')
     info = info.json()
-
     context = {'apod': info}
 
     return render(request, 'apod.html', context)
+
+
+def mars_rover_photos(request):
+    """Fotos de Marte"""
+
+    info = requests.get(
+        f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={api_key}')
+    info = info.json()
+
+    context = {'mrp': info}
+
+    return render(request, 'mars_rover_photos.html', context)
