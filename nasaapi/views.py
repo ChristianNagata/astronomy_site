5393e921d0781936e78c06d6dc75304088fc2b64
Christian <christian_nagata@hotmail.com>
2022-01-08 20:02:43 -0300

    organização da página apod, criação da página mrp e páginação

--- nasaapi/views.py
+++ nasaapi/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests
+from django.core.paginator import Paginator
 
 
 api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
@@ -8,7 +9,12 @@ api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
 def index(request):
     """Página inicial"""
 
-    return render(request, 'index.html')
+    info = requests.get(
+        f'https://api.nasa.gov/planetary/apod?api_key={api_key}')
+    info = info.json()
+    context = {'apod': info}
+
+    return render(request, 'index.html', context)
 
 
 def apod(request):
@@ -28,7 +34,13 @@ def mars_rover_photos(request):
     info = requests.get(
         f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={api_key}')
     info = info.json()
+    info = info['photos']
 
-    context = {'mrp': info}
+    # Paginação
+    paginator = Paginator(info, 18)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'mrp': page_obj}
 
     return render(request, 'mars_rover_photos.html', context)
