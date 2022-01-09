d67e68f1c7656f70ca3895282aa8d9570e2db6c7
Christian <christian_nagata@hotmail.com>
2022-01-09 18:30:13 -0300

    utilizando a api de notícias, modificação na página inicial

--- nasaapi/views.py
+++ nasaapi/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 import requests
 from django.core.paginator import Paginator
+from newsapi import NewsApiClient
 
 
-api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
+nasa_api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
+news_api_key = 'pub_34807a75f726f36d058d3cfe9af3f2201f82'
 
 
 def index(request):
     """Página inicial"""
 
+    # APOD API
     info = requests.get(
-        f'https://api.nasa.gov/planetary/apod?api_key={api_key}')
+        f'https://api.nasa.gov/planetary/apod?api_key={nasa_api_key}')
     info = info.json()
-    context = {'apod': info}
 
+    # NEWS API
+    language = 'en'
+    q = 'astronomy%20OR%20nasa%20OR%20spacex%20-nazi'
+    news = requests.get(
+        f'https://newsdata.io/api/1/news?apikey={news_api_key}&language={language}&q={q}')
+    news = news.json()
+    news = news['results']
+
+    context = {'apod': info, 'news': news}
     return render(request, 'index.html', context)
 
 
@@ -21,7 +32,7 @@ def apod(request):
     """Fotos atronômicas do dia"""
 
     info = requests.get(
-        f'https://api.nasa.gov/planetary/apod?api_key={api_key}')
+        f'https://api.nasa.gov/planetary/apod?api_key={nasa_api_key}')
     info = info.json()
     context = {'apod': info}
 
@@ -32,7 +43,7 @@ def mars_rover_photos(request):
     """Fotos de Marte"""
 
     info = requests.get(
-        f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={api_key}')
+        f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={nasa_api_key}')
     info = info.json()
     info = info['photos']
 
