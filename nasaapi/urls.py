96a473211455d100b0d81e3a78ed070b0afeea31
Christian <christian_nagata@hotmail.com>
2022-01-09 21:34:00 -0300

    url de apod alterada, ícone adicionado

--- nasaapi/urls.py
+++ nasaapi/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('APOD/', views.apod, name='apod'),
+    path('apod/', views.apod, name='apod'),
     path('MarsRoverPhotos/', views.mars_rover_photos, name='mars_rover_photos'),
 ]
