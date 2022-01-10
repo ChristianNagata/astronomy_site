f5393cacdf1844fb1c110680d9779e35e6c91c61
Christian <christian_nagata@hotmail.com>
2022-01-10 18:19:55 -0300

    criação de mars_rover_photo.html, e dinamismo na exibição de informações de cada página

--- nasaapi/urls.py
+++ nasaapi/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('apod/', views.apod, name='apod'),
     path('MarsRoverPhotos/', views.mars_rover_photos, name='mars_rover_photos'),
+    path('MarsRoverPhotos/<int:photo_id>/',
+         views.mars_rover_photo, name='mars_rover_photo'),
 ]
--- nasaapi/views.py
+++ nasaapi/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .apis import *
 from django.core.paginator import Paginator
+from types import SimpleNamespace
+import json
 
 
 def index(request):
@@ -12,7 +14,15 @@ def index(request):
     news = NewsAPI()
     news = news.news_api()
 
-    context = {'apod': info, 'news': news, 'page_name': 'Home'}
+    context = {
+        'apod': info,
+        'news': news,
+        'page_info': {
+            'name': 'Home',
+            'title': 'Stay informed about the Universe',
+            'description': 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.'
+        }
+    }
     return render(request, 'index.html', context)
 
 
@@ -22,7 +32,14 @@ def apod(request):
     info = NasaAPI()
     info = info.apod_api()
 
-    context = {'apod': info, 'page_name': 'APOD'}
+    context = {
+        'apod': info,
+        'page_info': {
+            'name': 'APOD',
+            'title': 'Astronomy Picture of the Day',
+            'description': 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.'
+        }
+    }
     return render(request, 'apod.html', context)
 
 
@@ -37,6 +54,34 @@ def mars_rover_photos(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    context = {'mrp': page_obj, 'page_name': 'Mars Photos'}
-
+    context = {
+        'mrp': page_obj,
+        'page_info': {
+            'name': 'Mars Rover Photos',
+            'title': 'Mars Rover Photos',
+            'description': 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.'
+        }
+    }
     return render(request, 'mars_rover_photos.html', context)
+
+
+def mars_rover_photo(request, photo_id):
+    """Foto de Marte"""
+
+    info = NasaAPI()
+    info = info.mrp_api()
+
+    for photo in info:
+        if photo['id'] == photo_id:
+            info = photo
+            break
+
+    context = {
+        'photo': info,
+        'page_info': {
+            'name': 'Mars Rover Photo',
+            'title': 'Mars Rover Photo',
+            'description': 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.'
+        }
+    }
+    return render(request, 'mars_rover_photo.html', context)
