8e254120e447d90408df1a3974926e0874c701dc
Christian <christian_nagata@hotmail.com>
2022-01-10 14:16:35 -0300

    refatoração em views.py e criação de apis.py

---
+++ nasaapi/apis.py
@@ -0,0 +1,41 @@
+import requests
+
+
+class NasaAPI:
+    """Retora as APIs da NASA"""
+
+    def __init__(self):
+        self.api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
+
+    def apod_api(self):
+        """APOD - Astronomy Photos of the Day"""
+
+        apod = requests.get(
+            f'https://api.nasa.gov/planetary/apod?api_key={self.api_key}')
+        apod = apod.json()
+        return apod
+
+    def mrp_api(self):
+        """Mars Rover Photos"""
+
+        mrp = requests.get(
+            f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={self.api_key}')
+        mrp = mrp.json()
+        mrp = mrp['photos']
+        return mrp
+
+
+class NewsAPI:
+    """Retorna a API de notícias"""
+
+    def __init__(self):
+        self.api_key = 'pub_34807a75f726f36d058d3cfe9af3f2201f82'
+
+    def news_api(self):
+        language = 'en'
+        q = 'astronomy%20OR%20nasa%20OR%20spacex%20-nazi'
+        news = requests.get(
+            f'https://newsdata.io/api/1/news?apikey={self.api_key}&language={language}&q={q}')
+        news = news.json()
+        news = news['results']
+        return news
--- nasaapi/views.py
+++ nasaapi/views.py
@@ -1,28 +1,16 @@
 from django.shortcuts import render
-import requests
+from .apis import *
 from django.core.paginator import Paginator
-from newsapi import NewsApiClient
-
-
-nasa_api_key = 'hKuFyqogFT5y2egU9TjUUdWrEd7PPc7vKmOB5PrY'
-news_api_key = 'pub_34807a75f726f36d058d3cfe9af3f2201f82'
 
 
 def index(request):
     """Página inicial"""
 
-    # APOD API
-    info = requests.get(
-        f'https://api.nasa.gov/planetary/apod?api_key={nasa_api_key}')
-    info = info.json()
+    info = NasaAPI()
+    info = info.apod_api()
 
-    # NEWS API
-    language = 'en'
-    q = 'astronomy%20OR%20nasa%20OR%20spacex%20-nazi'
-    news = requests.get(
-        f'https://newsdata.io/api/1/news?apikey={news_api_key}&language={language}&q={q}')
-    news = news.json()
-    news = news['results']
+    news = NewsAPI()
+    news = news.news_api()
 
     context = {'apod': info, 'news': news, 'page_name': 'Home'}
     return render(request, 'index.html', context)
@@ -31,21 +19,18 @@ def index(request):
 def apod(request):
     """Fotos atronômicas do dia"""
 
-    info = requests.get(
-        f'https://api.nasa.gov/planetary/apod?api_key={nasa_api_key}')
-    info = info.json()
-    context = {'apod': info, 'page_name': 'APOD'}
+    info = NasaAPI()
+    info = info.apod_api()
 
+    context = {'apod': info, 'page_name': 'APOD'}
     return render(request, 'apod.html', context)
 
 
 def mars_rover_photos(request):
     """Fotos de Marte"""
 
-    info = requests.get(
-        f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key={nasa_api_key}')
-    info = info.json()
-    info = info['photos']
+    info = NasaAPI()
+    info = info.mrp_api()
 
     # Paginação
     paginator = Paginator(info, 18)
